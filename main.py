421ed4ec780a8bc56b54af70ffceda381a2b4e3b
Luapulu <paul.nemec@tum.de>
2020-12-13 17:40:43 +0100

    Zero field splitting

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-print(np.genfromtxt("frequenzsweep.txt", delimiter=';', skip_header=14))
+zf_data = np.genfromtxt("frequenzsweep.txt", delimiter=';', skip_header=14)
 
+zf_omega = min(zip(zf_data[:, 1], zf_data[:, 3]), key=lambda t: t[1])[0]
 
+print(zf_omega)
 
-
-# hello world
+plt.scatter(zf_data[:, 1], zf_data[:, 3])
+plt.show()
