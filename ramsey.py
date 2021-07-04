b20436b9644cc72caa638cb849289a049b582e9f
Luapulu <paul.nemec@tum.de>
2021-07-04 13:14:50 +0200

    Fix ramsey

--- ramsey.py
+++ ramsey.py
@@ -2,12 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-ramsey_data = np.genfromtxt("echo.txt", delimiter=';', skip_header=14)
+ramsey_data = np.genfromtxt("ramsey1.txt", delimiter=';', skip_header=14)
 
 plt.plot(ramsey_data[:, 0], ramsey_data[:, 2])
 plt.xlabel("Time / ns")
 plt.ylabel("Average Intensity / count")
 
-plt.savefig("echo.png")
+plt.savefig("ramsey.png")
 
 plt.show()
